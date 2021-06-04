f56dbc4d86a55b4bd6aac2ad5ca0e57df8204c8e
Vanessa Su <vnessa.su@gmail.com>
2021-06-04 11:35:57 -0400

    :hammer: Add Flavor model

---
+++ PierresTreats/Models/Flavor.cs
@@ -0,0 +1,16 @@
+namespace PierresTreats.Models
+{
+  public class Flavor
+  {
+    public int FlavorId { get; set; }
+    public string Name { get; set; }
+
+    public virtual ICollection<FlavorTreat> FlavorTreatJoinEntities { get; }
+    public virtual ApplicationUser User { get; set; }
+
+    public Flavor()
+    {
+      this.FlavorTreatJoinEntities = new HashSet<FlavorTreat>();
+    }
+  }
+}
