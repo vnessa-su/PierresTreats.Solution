6a98dd0158fa26a2b121f8f3749fc4fd54aa350f
Vanessa Su <vnessa.su@gmail.com>
2021-06-04 13:20:25 -0400

    :hammer: Add database access and content to Home controller

--- PierresTreats/Controllers/HomeController.cs
+++ PierresTreats/Controllers/HomeController.cs
@@ -4,10 +4,18 @@ namespace PierresTreats.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
 
     [HttpGet("/")]
     public ActionResult Index()
     {
+      ViewBag.TreatList = _db.Treats.ToList();
+      ViewBag.FlavorList = _db.Flavors.ToList();
       return View();
     }
 
