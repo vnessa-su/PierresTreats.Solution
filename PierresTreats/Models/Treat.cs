3f1619e5882c48b97866ec901a09a101bc2334e0
Vanessa Su <vnessa.su@gmail.com>
2021-06-04 11:33:10 -0400

    :hammer: Add properties to Treat model

--- PierresTreats/Models/Treat.cs
+++ PierresTreats/Models/Treat.cs
@@ -2,6 +2,17 @@ namespace PierresTreats.Models
 {
   public class Treat
   {
-    //properties
+    public int TreatId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+
+    public virtual ICollection<FlavorTreat> FlavorTreatJoinEntities { get; }
+    public virtual ApplicationUser User { get; set; }
+
+    public Treat()
+    {
+      this.FlavorTreatJoinEntities = new HashSet<FlavorTreat>();
+    }
   }
 }
