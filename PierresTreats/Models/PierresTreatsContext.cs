a2692043b53230fd03ac125df13a9b222dc27fa6
Vanessa Su <vnessa.su@gmail.com>
2021-06-04 11:43:59 -0400

    :hammer: Add tables and Identity to context

--- PierresTreats/Models/PierresTreatsContext.cs
+++ PierresTreats/Models/PierresTreatsContext.cs
@@ -1,10 +1,13 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace PierresTreats.Models
 {
-  public class PierresTreatsContext : DbContext
+  public class PierresTreatsContext : IdentityDbContext<ApplicationUser>
   {
-    public virtual DbSet<Model> Models { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<FlavorTreat> FlavorTreat { get; set; }
 
     public PierresTreatsContext(DbContextOptions options) : base(options) { }
 
